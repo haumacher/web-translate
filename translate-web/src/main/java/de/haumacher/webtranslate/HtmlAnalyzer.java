9a3091cef7a03e1a735b936644d9ed81730df8da
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2025-03-23 12:41:46 +0100

    Use custom attribute for text ids.

--- translate-web/src/main/java/de/haumacher/webtranslate/HtmlAnalyzer.java
+++ translate-web/src/main/java/de/haumacher/webtranslate/HtmlAnalyzer.java
@@ -2,7 +2,9 @@ package de.haumacher.webtranslate;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,9 +15,13 @@ import org.w3c.dom.Text;
 
 public class HtmlAnalyzer {
 	
+	private static final String ID_ATTR = "data-tx";
+
 	private static final Pattern textIdPattern = Pattern.compile("t0*([1-9]\\d+)");
 
 	private Document document;
+	
+	private Set<Element> textParents = new HashSet<>();
 	private Map<String, Element> elementById = new HashMap<>();
 	private int nextId = 1;
 	private DecimalFormat idFormat = new DecimalFormat("t0000");
@@ -28,14 +34,51 @@ public class HtmlAnalyzer {
 	public void analyze() {
 		scanIds(document.getDocumentElement());
 		scanText(document.getDocumentElement());
+		assignIds();
+		cleanIds(document.getDocumentElement());
 	}
 	
+	private void cleanIds(Element element) {
+		for (Node child = element.getFirstChild(); child != null; child = child.getNextSibling()) {
+			if (child instanceof Element sub) {
+				if (!textParents.contains(child)) {
+					((Element) child).removeAttribute(ID_ATTR);
+				}
+				cleanIds(sub);
+			}
+		}
+	}
+
+	private void assignIds() {
+		for (Element textParent : textParents) {
+			if (hasTextParent(textParent)) {
+				continue;
+			}
+			
+			String id = textParent.getAttribute(ID_ATTR);
+			if (id == null || id.isBlank()) {
+				id = idFormat.format(nextId ++);
+				textParent.setAttribute(ID_ATTR, id);
+			}
+			
+			elementById.put(id, textParent);
+		}
+	}
+
+	private boolean hasTextParent(Element textParent) {
+		for (Node parent = textParent.getParentNode(); parent != null; parent = parent.getParentNode()) {
+			if (textParents.contains(parent)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	// <p id="t0040">Some text<b><i>what</i> a great</b> nonsense.</p>
 
 	private void scanIds(Element element) {
-		String id = element.getAttribute("id");
+		String id = element.getAttribute(ID_ATTR);
 		if (id != null && !id.isEmpty()) {
-			elementById.put(id, element);
 			Matcher matcher = textIdPattern.matcher(id);
 			if (matcher.matches()) {
 				nextId = Math.max(nextId, Integer.parseInt(matcher.group(1)) + 1);
@@ -53,17 +96,32 @@ public class HtmlAnalyzer {
 		for (Node child = element.getFirstChild(); child != null; child = child.getNextSibling()) {
 			if (child instanceof Text text) {
 				String txt = text.getTextContent();
-				if (txt != null && !txt.isBlank()) {
+				if (isText(txt)) {
 					Element textParent = (Element) child.getParentNode();
-					String id = textParent.getAttribute("id");
-					if (id == null || !id.isBlank()) {
-						textParent.setAttribute("id", idFormat.format(nextId ++));
-					}
+					textParents.add(textParent);
 				}
 			}
 			else if (child instanceof Element sub) {
 				scanText(sub);
 			}
 		}
+	}
+
+	private boolean isText(String txt) {
+		if (txt == null) {
+			return false;
+		}
+		if (txt.isBlank()) {
+			return false;
+		}
+		
+		for (int n = 0, len = txt.length(); n < len; n++) {
+			char ch = txt.charAt(n);
+			if (Character.isLetter(ch)) {
+				return true;
+			}
+		}
+		
+		return true;
 	}
 }
--- translate-web/src/main/java/de/haumacher/webtranslate/Main.java
+++ translate-web/src/main/java/de/haumacher/webtranslate/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -11,6 +12,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSOutput;
 import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class Main {
@@ -18,7 +21,16 @@ public class Main {
 	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
 		String fileName = args[0];
 		
-		Document document = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().parse(new File(fileName));
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		EntityResolver resolver = new EntityResolver() {
+			@Override
+			public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+				return null;
+			}
+		};
+		builder.setEntityResolver(resolver);
+		Document document = builder.parse(new File(fileName));
 
 		new HtmlAnalyzer(document).analyze();
 		
@@ -26,8 +38,8 @@ public class Main {
 	    final LSSerializer lsSerializer = domImplementation.createLSSerializer();
 	    LSOutput output = domImplementation.createLSOutput();
 	    
-	    String baseName = fileName.endsWith(".xml") ? fileName.substring(0, fileName.length() - 4) : fileName;
-		try (FileOutputStream out = new FileOutputStream(new File(baseName + "-id.xml"))) {
+	    String baseName = fileName.endsWith(".html") ? fileName.substring(0, fileName.length() - ".html".length()) : fileName;
+		try (FileOutputStream out = new FileOutputStream(new File(baseName + "-id.html"))) {
 	    	output.setByteStream(out);
 	    	lsSerializer.write(document, output);
 	    }
